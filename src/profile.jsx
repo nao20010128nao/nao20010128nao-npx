71e29942f018a4b371a7b34134707b035a776ecc
nao20010128nao <nao20010128@gmail.com>
2018-06-13 01:29:33 +0900

    リンク集

--- src/profile.jsx
+++ src/profile.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 
-export class Profile extends Component{
+export class Profile extends Component {
     render() {
         return (
-          <box label="Profile"
-               left='2%'
-               top="5%"
-               width='45%'
-               height="45%"
-               border={{type: 'line'}}
-               style={{border: {fg: 'green'}}}>
-            {"Birthday: 28 Jan, 2018\nGender: Male"}
-          </box>
+            <box label="Profile"
+                left='2%'
+                top="5%"
+                width='45%'
+                height="45%"
+                border={{ type: 'line' }}
+                style={{ border: { fg: 'green' } }}>
+                <box
+                    left='5%'
+                    top="5%"
+                    width='90%'
+                    height="80%">
+                    {"Birthday: 28 Jan, 2018\nGender: Male"}
+                </box>
+            </box>
         );
     }
 }
