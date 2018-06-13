193211250b65bd6093b14739eb2377f91a4ea57f
nao20010128nao <nao20010128@gmail.com>
2018-06-13 23:47:15 +0900

    なわけがあるか
    This reverts commit 747f79477857b7fd3a753a938e6f3de2c20effe9.

---
+++ src/profile.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export class Profile extends Component {
+    render() {
+        return (
+            <box label="Profile"
+                left='2%'
+                top="5%"
+                width='45%'
+                height="45%"
+                border={{ type: 'line' }}
+                style={{ border: { fg: 'green' } }}>
+                <box
+                    left='5%'
+                    top="5%"
+                    width='90%'
+                    height="80%">
+                    {"Birthday: 28 Jan, 2001\nGender: Male\nNot a general adult man"}
+                </box>
+            </box>
+        );
+    }
+}
