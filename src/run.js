747f79477857b7fd3a753a938e6f3de2c20effe9
nao20010128nao <nao20010128@gmail.com>
2018-06-13 21:36:18 +0900

    Ink使って根本的に書き換え

--- src/run.js
+++ src/run.js
@@ -1,5 +1,24 @@
-#!/usr/bin/env node
-require('babel-register')({
-    "presets": ["react", "es2015"]
-});
-require("./index");
+#!/usr/bin/node
+
+'use strict';
+const path = require('path');
+const meow = require('meow');
+const termImg = require('term-img');
+const importJsx = require('import-jsx');
+const terminalImage = require('terminal-image');
+const {
+    h,
+    render
+} = require('ink');
+
+const ui = importJsx('./ui');
+const avatar = path.join(__dirname, 'avatar.png');
+
+meow(`
+	Usage
+	  $ nao20010128nao
+`);
+
+terminalImage.file(avatar)
+    .then(console.log)
+    .then(() => render(h(ui)));
